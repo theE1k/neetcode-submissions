9c0388c4d4e939614dacf4ff178f783c9f0b1d4a
theE1k <77948854+theE1k@users.noreply.github.com>
2026-09-19 00:12:02 +0100

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n <0:
+            x = 1/x
+            n = -n
+    
+        def recursive(x,n):
+            if n == 0:
+                return 1
+           
+            if n & 1:
+                return recursive(x*x,n//2) * x
+            return recursive(x*x,n//2) 
+        
+        return recursive(x,n)
+            
